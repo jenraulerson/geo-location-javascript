5344e61ddb696be0588971e64ae70bcecb024ae1
whoisstan@gmail.com <whoisstan@gmail.com@d8ed5e44-6ce0-11de-bda8-898f317151bd>
2011-01-11 18:26:55 +0000

    chrome issue fix

--- trunk/js/geo.js
+++ trunk/js/geo.js
@@ -1,36 +1,36 @@
-/*!
- * geo-location-javascript v0.4.3
- * http://code.google.com/p/geo-location-javascript/
- *
- * Copyright (c) 2009 Stan Wiechers
- * Licensed under the MIT licenses.
- *
- * Revision: $Rev$: 
- * Author: $Author$:
- * Date: $Date$:    
- */
-var bb_successCallback;
-var bb_errorCallback;
+//
+//geo-location-javascript v0.4.8
+//http://code.google.com/p/geo-location-javascript/
+//
+//Copyright (c) 2009 Stan Wiechers
+//Licensed under the MIT licenses.
+//
+//Revision: $Rev$: 
+//Author: $Author$:
+//Date: $Date$:    
+//
+var bb_success;
+var bb_error;
 var bb_blackberryTimeout_id=-1;
 
 function handleBlackBerryLocationTimeout()
 {
 	if(bb_blackberryTimeout_id!=-1)
 	{
-		bb_errorCallback({message:"Timeout error", code:3});
+		bb_error({message:"Timeout error", code:3});
 	}
 }
 function handleBlackBerryLocation()
 {
 		clearTimeout(bb_blackberryTimeout_id);
 		bb_blackberryTimeout_id=-1;
-        if (bb_successCallback && bb_errorCallback)
+        if (bb_success && bb_error)
         {
                 if(blackberry.location.latitude==0 && blackberry.location.longitude==0)
                 {
                         //http://dev.w3.org/geo/api/spec-source.html#position_unavailable_error
                         //POSITION_UNAVAILABLE (numeric value 2)
-                        bb_errorCallback({message:"Position unavailable", code:2});
+                        bb_error({message:"Position unavailable", code:2});
                 }
                 else
                 {  
@@ -41,14 +41,14 @@ function handleBlackBerryLocation()
                         {
                                 timestamp=new Date(blackberry.location.timestamp);
                         }
-                        bb_successCallback({timestamp:timestamp, coords: {latitude:blackberry.location.latitude,longitude:blackberry.location.longitude}});
+                        bb_success({timestamp:timestamp, coords: {latitude:blackberry.location.latitude,longitude:blackberry.location.longitude}});
                 }
                 //since blackberry.location.removeLocationUpdate();
                 //is not working as described http://na.blackberry.com/eng/deliverables/8861/blackberry_location_removeLocationUpdate_568409_11.jsp
                 //the callback are set to null to indicate that the job is done
 
-                bb_successCallback = null;
-                bb_errorCallback = null;
+                bb_success = null;
+                bb_error = null;
         }
 }
 
@@ -57,10 +57,11 @@ var geo_position_js=function() {
         var pub = {};
         var provider=null;
 
-        pub.getCurrentPosition = function(successCallback,errorCallback,options)
+        pub.getCurrentPosition = function(success,error,opts)
         {
-                provider.getCurrentPosition(successCallback, errorCallback,options);
+                provider.getCurrentPosition(success, error,opts);
         }
+		
 
         pub.init = function()
         {			
@@ -77,52 +78,86 @@ var geo_position_js=function() {
                         else if (typeof(navigator.geolocation)!="undefined")
                         {
                                 provider=navigator.geolocation;
-                                pub.getCurrentPosition = function(successCallback, errorCallback, options)
+                                pub.getCurrentPosition = function(success, error, opts)
                                 {
-                                        function _successCallback(p)
+                                        function _success(p)
                                         {
                                                 //for mozilla geode,it returns the coordinates slightly differently
                                                 if(typeof(p.latitude)!="undefined")
                                                 {
-                                                        successCallback({timestamp:p.timestamp, coords: {latitude:p.latitude,longitude:p.longitude}});
+                                                        success({timestamp:p.timestamp, coords: {latitude:p.latitude,longitude:p.longitude}});
                                                 }
                                                 else
                                                 {
-                                                        successCallback(p);
+                                                        success(p);
                                                 }
                                         }
-                                        provider.getCurrentPosition(_successCallback,errorCallback,options);
+                                        provider.getCurrentPosition(_success,error,opts);
                                 }
                         }
-                         else if(typeof(window.google)!="undefined" && typeof(google.gears)!="undefined")
+                        else if(typeof(window.blackberry)!="undefined" && blackberry.location.GPSSupported)
+                        {
+
+                                // set to autonomous mode
+								if(typeof(blackberry.location.setAidMode)=="undefined")
+								{
+	                                return false;									
+								}
+								blackberry.location.setAidMode(2);
+                                //override default method implementation
+                                pub.getCurrentPosition = function(success,error,opts)
+                                {
+										//alert(parseFloat(navigator.appVersion));
+                                        //passing over callbacks as parameter didn't work consistently
+                                        //in the onLocationUpdate method, thats why they have to be set
+                                        //outside
+                                        bb_success=success;
+                                        bb_error=error;
+                                        //function needs to be a string according to
+                                        //http://www.tonybunce.com/2008/05/08/Blackberry-Browser-Amp-GPS.aspx
+										if(opts['timeout'])  
+										{
+										 	bb_blackberryTimeout_id=setTimeout("handleBlackBerryLocationTimeout()",opts['timeout']);
+										}
+										else
+										//default timeout when none is given to prevent a hanging script
+										{
+											bb_blackberryTimeout_id=setTimeout("handleBlackBerryLocationTimeout()",60000);
+										}										
+										blackberry.location.onLocationUpdate("handleBlackBerryLocation()");
+                                        blackberry.location.refreshLocation();
+                                }
+                                provider=blackberry.location;				
+                        }
+                        else if(typeof(window.google)!="undefined" && typeof(google.gears)!="undefined")
                         {
                                 provider=google.gears.factory.create('beta.geolocation');
                         }
                         else if ( typeof(Mojo) !="undefined" && typeof(Mojo.Service.Request)!="Mojo.Service.Request")
                         {
                                 provider=true;
-                                pub.getCurrentPosition = function(successCallback, errorCallback, options)
+                                pub.getCurrentPosition = function(success, error, opts)
                                 {
 
                                 parameters={};
-                                if(options)
+                                if(opts)
                                 {
                                          //http://developer.palm.com/index.php?option=com_content&view=article&id=1673#GPS-getCurrentPosition
-                                         if (options.enableHighAccuracy && options.enableHighAccuracy==true)
+                                         if (opts.enableHighAccuracy && opts.enableHighAccuracy==true)
                                          {
                                                 parameters.accuracy=1;
                                          }
-                                         if (options.maximumAge)
+                                         if (opts.maximumAge)
                                          {
-                                                parameters.maximumAge=options.maximumAge;
+                                                parameters.maximumAge=opts.maximumAge;
                                          }
-                                         if (options.responseTime)
+                                         if (opts.responseTime)
                                          {
-                                                if(options.responseTime<5)
+                                                if(opts.responseTime<5)
                                                 {
                                                         parameters.responseTime=1;
                                                 }
-                                                else if (options.responseTime<20)
+                                                else if (opts.responseTime<20)
                                                 {
                                                         parameters.responseTime=2;
                                                 }
@@ -137,19 +172,19 @@ var geo_position_js=function() {
                                  r=new Mojo.Service.Request('palm://com.palm.location', {
                                         method:"getCurrentPosition",
                                             parameters:parameters,
-                                            onSuccess: function(p){successCallback({timestamp:p.timestamp, coords: {latitude:p.latitude, longitude:p.longitude,heading:p.heading}});},
+                                            onSuccess: function(p){success({timestamp:p.timestamp, coords: {latitude:p.latitude, longitude:p.longitude,heading:p.heading}});},
                                             onFailure: function(e){
                                                                 if (e.errorCode==1)
                                                                 {
-                                                                        errorCallback({code:3,message:"Timeout"});
+                                                                        error({code:3,message:"Timeout"});
                                                                 }
                                                                 else if (e.errorCode==2)
                                                                 {
-                                                                        errorCallback({code:2,message:"Position Unavailable"});
+                                                                        error({code:2,message:"Position Unavailable"});
                                                                 }
                                                                 else
                                                                 {
-                                                                        errorCallback({code:0,message:"Unknown Error: webOS-code"+errorCode});
+                                                                        error({code:0,message:"Unknown Error: webOS-code"+errorCode});
                                                                 }
                                                         }
                                             });
@@ -161,17 +196,17 @@ var geo_position_js=function() {
                                 provider=device.getServiceObject("Service.Location", "ILocation");
 
                                 //override default method implementation
-                                pub.getCurrentPosition = function(successCallback, errorCallback, options)
+                                pub.getCurrentPosition = function(success, error, opts)
                                 {
                                         function callback(transId, eventCode, result) {
                                             if (eventCode == 4)
                                                 {
-                                                errorCallback({message:"Position unavailable", code:2});
+                                                error({message:"Position unavailable", code:2});
                                             }
                                                 else
                                                 {
                                                         //no timestamp of location given?
-                                                        successCallback({timestamp:null, coords: {latitude:result.ReturnValue.Latitude, longitude:result.ReturnValue.Longitude, altitude:result.ReturnValue.Altitude,heading:result.ReturnValue.Heading}});
+                                                        success({timestamp:null, coords: {latitude:result.ReturnValue.Latitude, longitude:result.ReturnValue.Longitude, altitude:result.ReturnValue.Altitude,heading:result.ReturnValue.Heading}});
                                                 }
                                         }
                                         //location criteria
@@ -181,43 +216,9 @@ var geo_position_js=function() {
                                         provider.ILocation.GetLocation(criteria,callback);
                                 }
                         }
-                        else if(typeof(window.blackberry)!="undefined" && blackberry.location.GPSSupported)
-                        {
 
-                                // set to autonomous mode
-								if(typeof(blackberry.location.setAidMode)=="undefined")
-								{
-	                                return false;									
-								}
-								blackberry.location.setAidMode(2);
-                                //override default method implementation
-                                pub.getCurrentPosition = function(successCallback,errorCallback,options)
-                                {
-										//alert(parseFloat(navigator.appVersion));
-                                        //passing over callbacks as parameter didn't work consistently
-                                        //in the onLocationUpdate method, thats why they have to be set
-                                        //outside
-                                        bb_successCallback=successCallback;
-                                        bb_errorCallback=errorCallback;
-                                        //function needs to be a string according to
-                                        //http://www.tonybunce.com/2008/05/08/Blackberry-Browser-Amp-GPS.aspx
-										if(options['timeout'])  
-										{
-										 	bb_blackberryTimeout_id=setTimeout("handleBlackBerryLocationTimeout()",options['timeout']);
-										}
-										else
-										//default timeout when none is given to prevent a hanging script
-										{
-											bb_blackberryTimeout_id=setTimeout("handleBlackBerryLocationTimeout()",60000);
-										}										
-										blackberry.location.onLocationUpdate("handleBlackBerryLocation()");
-                                        blackberry.location.refreshLocation();
-                                }
-                                provider=blackberry.location;				
-                        }
                 }
                 catch (e){ 
-					alert("error="+e);
 					if(typeof(console)!="undefined")
 					{
 						console.log(e);
